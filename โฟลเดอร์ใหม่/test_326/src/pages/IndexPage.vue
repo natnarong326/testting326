2fd95d267047ef417a8420e20d75280b2d85df88
natnarong326 <nxtnx09@gmail.com>
2024-09-07 00:07:49 +0700

    Add files via upload

---
+++ โฟลเดอร์ใหม่/test_326/src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ head }}</h1>
+    <form>
+      <input type="text" id="firstName" v-model="firstName" />
+      <input type="text" id="secondName" v-model="secondName" />
+      <input type="text" id="Codename" v-model="Codename" />
+      <input type="text" id="nickname" v-model="nickname" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputPage',
+  data() {
+    return {
+      head: 'Vue is awesome.',
+      firstName: 'นายณัฐณรงค์ ',
+      secondName: 'คูละสะ',
+      Codename:'6604101326',
+      nickname:'ติ้ก',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
